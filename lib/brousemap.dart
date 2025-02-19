e84c18455cd90165539802b639e0d52427397df6
samaraEdirisooriya <103551067+samaraEdirisooriya@users.noreply.github.com>
2025-02-19 20:46:50 +0530

    scooter

--- lib/brousemap.dart
+++ lib/brousemap.dart
@@ -26,28 +26,31 @@ class _MapScreenState extends State<MapScreen> {
   void dispose() {
     _mapController.dispose();
     super.dispose();
-
   }
 
- void fetchScooter() {
-  _database.onValue.listen((event) {
-    final data = event.snapshot.value as Map<dynamic, dynamic>?;
-    if (data != null) {
-      print("Data from Firebase: $data");
-      if (data["availability"] == true) {
-        setState(() {
-          scooterData = {
-            "name": data["name"],
-            "lat": data["lat"],
-            "lang": data["lang"],
-            "owneruid": data["owneruid"],
-            "parkingopen": data["parkingopen"],
-          };
-        });
+  void fetchScooter() {
+    _database.onValue.listen((event) {
+      final data = event.snapshot.value as Map<dynamic, dynamic>?;
+      if (data != null) {
+        print("Data from Firebase: $data");
+        if (data["availability"] == true) {
+          setState(() {
+            scooterData = {
+              "name": data["name"],
+              "lat": data["lat"],
+              "lang": data["lang"],
+              "owneruid": data["owneruid"],
+              "parkingopen": data["parkingopen"],
+            };
+             _mapController.move(
+            LatLng(scooterData!['lat'], scooterData!['lang']),
+            18, // Zoom level (adjust as needed)
+          );
+          });
+        }
       }
-    }
-  });
-}
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,20 +79,19 @@ class _MapScreenState extends State<MapScreen> {
       mapOptions: MapOptions(
         minZoom: 15,
         maxZoom: 18,
-        initialZoom: 15,
+        initialZoom: 19,
         initialCenter: LatLng(8.361005073025808, 80.50334762480115), // Default initial location
       ),
-      markers:[
-       
-        Marker(
-          point: LatLng(scooterData!['lat'], scooterData!['lang']),
-          child: const Icon(
-            size: 40.0,
-            color: Colors.red,
-            Icons.location_on_rounded,
-          ),
-        ),
-      ], // Pass the markers as a list
+      markers: scooterData != null
+          ? [
+              Marker(
+                width: 100,
+                height: 100,
+                point: LatLng(scooterData!['lat'], scooterData!['lang']),
+                child: Image.asset("images/New Project (10).png",)
+              ),
+            ]
+          : [], // If scooterData is null, no markers are added
     );
   }
 }
